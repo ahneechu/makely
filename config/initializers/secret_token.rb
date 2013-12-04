8fa9ee1e6ea08b9edf5beb71abd050ff4f9357e4
Anne Chu <ahnee.chu@gmail.com>
2013-12-04 11:23:32 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makely::Application.config.secret_key_base = '05257825a0bd27450769f9a44b678a86d42ee6e110826f7187d14d66e60bf89f40a69394738106f94bc3a0409d6f4262558b5a02941d95f459e3832adaa5bfd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Makely::Application.config.session_store :cookie_store, key: '_makely_session'
