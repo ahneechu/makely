357998396c923010ea6df28fb59cf8e5e0f4d48d
Will Spearing <nwspearing@gmail.com>
2013-12-07 18:15:12 -0800

    Orders Crud now working

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,17 +4,22 @@ before_action :set_order, only: [:show, :edit, :update, :destroy]
 
 	def new
 		# @customer_id = params[:customer_id]
-		@customer = Customer.find(params[:customer_id])
+		# @customer = Customer.find(params[:customer_id])
+		customer = Customer.find(params[:customer_id])
 		# @order = Order.new
-		@order = Order.new
+		@order = customer.orders.build
+
+		# @order = Order.new
 	end
 
 
 	def index
-		@customer = Customer.find(params[:customer_id])
+		customer = Customer.find(params[:customer_id])
+		# @customer = Customer.find(params[:customer_id])
 		# @orders = Order.all
 		# @orders = customer.order.all
-		@orders = @customer.orders
+		# @orders = @customer.orders
+		@orders = customer.orders
 		# @orders = current_customer_user.orders
 		# @orders = Customer.find_by(customer_id: params[:customer_id])
 		# @orders = @customer.orders
@@ -22,19 +27,23 @@ before_action :set_order, only: [:show, :edit, :update, :destroy]
 
 
 	def show
-		@customer = Customer.find(params[:customer_id])
-	end
+		# @customer = Customer.find(params[:customer_id])
+	end 
 
 
 	def create
-		@customer = Customer.find(params[:customer_id])
+		# @customer = Customer.find(params[:customer_id])
+		customer = Customer.find(params[:customer_id])
+		@order = customer.orders.create(order_params)
+
 		# @order = Order.new(params.require(:order).permit(order_params))
-		@order = Order.new(params[:order].permit(:order_number, :ordered_date, :pending_date, :completed_date, :cancel_date, :archived_state, :product_purchased, :customer_id, :store_id))
+		# @order = Order.new(params[:order].permit(:order_number, :ordered_date, :pending_date, :completed_date, :cancel_date, :archived_state, :product_purchased, :customer_id, :store_id))
 				# @order = Order.new(params.require(:order).permit(:order_number, :ordered_date, :pending_date, :completed_date, :cancel_date, :archived_state, :product_purchased, :customer_id, :store_id))
 
 		if @order.save
+			redirect_to([@order.customer, @order])
 			# redirect_to customer_order_url(@customer)
-			redirect_to customer_order_url(@customer, @order)
+			# redirect_to customer_order_url(@customer, @order)
 			# redirect_to "/customers/5/orders"
 			# redirect_to customer_order_url
 			# redirect_to customers_url
@@ -47,14 +56,17 @@ before_action :set_order, only: [:show, :edit, :update, :destroy]
 	  	end
 
 	  	def update
-	  		@order.update(order_params)
-	  		redirect_to orders_url
+	  		if @order.update_attributes(order_params)
+	  		# @order.update(order_params)
+	  		redirect_to customer_orders_url
+	  	end
 	  	end
 
 	  	 def destroy
-	  	 	Order.find(params[:id]).destroy
+	  	 	@order.destroy
+	  	 	# Order.find(params[:id]).destroy
     		# @order.destroy
-    		redirect_to orders_url
+    		redirect_to customer_orders_url
   		end
 
 
@@ -62,7 +74,9 @@ private
     # Use callbacks to share common setup or constraints between actions.
 
     def set_order
-      @order = Order.find(params[:id])
+    	customer = Customer.find(params[:customer_id])
+    	@order = customer.orders.find(params[:id])
+      # @order = Order.find(params[:id])
           # respond_with (@orders)
 
 
